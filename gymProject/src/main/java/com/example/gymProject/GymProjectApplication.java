02e53d5f99bf6e15937bfdcd7eeb54f17970024f
DESKTOP-ASBCGPU\comic <intinan12345@gmail.com>
2022-12-20 15:09:31 +0900

    update test

--- gymProject/src/main/java/com/example/gymProject/GymProjectApplication.java
+++ gymProject/src/main/java/com/example/gymProject/GymProjectApplication.java
@@ -4,9 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 @SpringBootApplication
 public class GymProjectApplication {
 	protected static Logger logger = LoggerFactory.getLogger(GymProjectApplication.class.getName());
