5ffe5f4691ecb2e5fdfeedfb5d8ead3387f49c1a
DESKTOP-ASBCGPU\comic <intinan12345@gmail.com>
2022-12-20 15:41:00 +0900

    1234

--- gymProject/src/main/java/com/example/gymProject/GymProjectApplication.java
+++ gymProject/src/main/java/com/example/gymProject/GymProjectApplication.java
@@ -1,17 +1,15 @@
 package com.example.gymProject;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
 public class GymProjectApplication {
-	protected static Logger logger = LoggerFactory.getLogger(GymProjectApplication.class.getName());
+	//protected static Logger logger = LoggerFactory.getLogger(GymProjectApplication.class.getName());
 	public static void main(String[] args) {
 		//SpringApplication.run(GymProjectApplication.class, args);
 		//System.out.println("daw");
-		logger.debug("sda");
+		//logger.debug("sda");
 	}
 
 }
