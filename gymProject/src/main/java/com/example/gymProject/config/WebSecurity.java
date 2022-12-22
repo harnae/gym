27ef71934dbcce5414ce1795fde60855c3bcb246
DESKTOP-ASBCGPU\comic <intinan12345@gmail.com>
2022-12-22 14:08:40 +0900

    security config

--- gymProject/src/main/java/com/example/gymProject/config/WebSecurity.java
+++ gymProject/src/main/java/com/example/gymProject/config/WebSecurity.java
@@ -1,30 +1,43 @@
 package com.example.gymProject.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-//@EnableWebSecurity
+@EnableWebSecurity
 public class WebSecurity {
 //	@Bean
 //	public WebSecurityCustomizer webSecurityCustomizer(WebSecurity web) {
 //		return web -> web.ignoring().requestMatchers("/html/css");
 //	}
 
+	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-	    return httpSecurity
-	        .csrf(csrf -> csrf.disable())
-	        .authorizeHttpRequests(auth -> auth
-	            .requestMatchers("/").permitAll()
-	            .anyRequest().authenticated()
-	        )
-	        .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-	        .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
-	        .httpBasic(Customizer.withDefaults())
-	        .build();
+//	    return httpSecurity
+//	        .csrf(csrf -> csrf.disable())
+//	        .authorizeHttpRequests(auth -> auth
+//	            .requestMatchers("/").permitAll()
+//	            .anyRequest().authenticated()
+//	        )
+//	        .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//	        .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+//	        .httpBasic(Customizer.withDefaults())
+//	        .build();
+		return httpSecurity
+				.authorizeHttpRequests(authco -> authco
+						.requestMatchers("/css/**")
+						.permitAll()
+						.requestMatchers("/")
+						.permitAll()
+						)
+				.build();
+
 	}
+
+
+
+
 }
--- gymProject/src/main/java/com/example/gymProject/controller/ListController.java
+++ gymProject/src/main/java/com/example/gymProject/controller/ListController.java
@@ -22,6 +22,6 @@ public class ListController {
 	public String index() {
 		System.out.println(ls.getUserList());
 
-		return "login/login";
+		return "/login/login";
 	}
 }
